1eb9381e2831f2e1839852896604617bb678f4be
wensboy <wendisx158@163.com>
2024-11-16 20:34:32 +0800

    add array

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# stl_lib
-About C++ standard template library
+## 关于stl of C plus plus 的学习记录
+
+包括如下内容：
+
+>容器
+>>array
+>>deque
+>>forward_list
+>>list
+>>map
+>>queue
+>>set
+>>stack
+>>unordered_map
+>>unordered_set
+>>vector
+>
+>I/O
+>>fstream
+>>iomanip
+>>ios
+>>iosfwd
+>>iostream
+>>istream
+>>ostream
+>>sstream
+>>streambuf
+>
+>算法
+>>algorithm
+>
+>迭代器
+>>iterator
+>
+>多线程
+>>atomic
+>>condition_variable
+>>future
+>>mutex
+>>thread
+>
+>其他
---
+++ containers/array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<array>
+
+/*
+<T>::at(int n)-->return element at the specified position
+<T>::back()-->return the last element reference
+<T>::begin()-->returns an iterator pointing to the first element
+<T>::cbegin()-->returns a const_iterator pointing to the first element
+<T>::cend()-->returns a const_iterator pointing to the past-the-end element
+<T>::crbegin()-->returns a const_reverse_iterator pointing to the last element
+<T>::crend()-->returns a const_reverse_iterator pointing to the theoretical element preceding the first element
+<T>::data()-->returns a pointer to the first element
+<T>::empty()-->returns a bool value 
+<T>::end()-->returns an iterator pointing to the past-the-end element
+void::fill(<T> val)-->sets val as the value for all the elements
+<T>::front()-->returns a reference to the first element
+int::max_size()-->returns the maximum number of elements
+<T>::operator[](int n)-->returns a reference to the element at position n
+<T>::rbegin()-->returns a reverse iterator pointing to the last element
+<T>::rend()-->returns a reverse iterator pointing to the theoretical element preceding the first element
+int::size()-->returns the number of elements
+void::swap(array& x)-->exchanges the content of the array by the content of x
+*/
+
+int main(){
+    std::cout<<"array"<<std::endl;
+    return 0;
+}
